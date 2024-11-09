636e461f17a8ea14a758545d0d7a3b8a06245b9d
tomtomApp <mtom0809tom0809@gmail.com>
2024-11-09 20:12:41 +0900

    [fix]setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ package_name = "decision_controller"
 setup(
     name=package_name,
     version="0.1.0",
-    packages=find_packages(include=[package_name, 'python_programs', 'python_programs.*']),
+    packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', glob('launch/*.py')),  # launchファイルを含める
     ],
     install_requires=["setuptools"],
     zip_safe=True,
@@ -22,8 +22,8 @@ setup(
     license="Apache License 2.0",
     tests_require=["pytest"],
     entry_points={
-        "console_scripts": [
-            "decision_control_node = python_programs.decision_control_node:main"
+    'console_scripts': [
+        'decision_control_node = decision_controller.decision_control_node:main'
         ],
     },
 )
